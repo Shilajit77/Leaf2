bc31e8cd03608c555418dfe0fa87c01e0bf4ea6f
Shilajit77 <111560092+Shilajit77@users.noreply.github.com>
2023-05-02 22:45:35 +0530

    Update trying.py

--- trying.py
+++ trying.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import pickle
-pip install torch
+!pip install torch
 import torch
 from torchvision import models
 import torch.nn as nn
